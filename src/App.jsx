a52bf820ad6ccdf50f013f314c9a9bfff656f4f0
Yash <yashshukla336@gmail.com>
2024-02-03 19:44:30 +0530

    added new modal for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,48 @@
-import { useState } from "react";
+import { Network } from "@capacitor/network";
+
+import { useEffect, useState } from "react";
 import Modal from "./Components/Web/Modal";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import MobileModal from "./Components/Mobile/Modal.Mobile";
 
 function App() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isOpen, setOpen] = useState(false);
+  const [isOnline, setOnline] = useState(true);
+
+  console.log("show", isOnline);
+
+  useEffect(() => {
+    const handleNetworkChange = (status) => {
+      setOnline(status?.connected);
+    };
+
+    const CheckNetwork = async () => {
+      const status = await Network.getStatus();
+      handleNetworkChange(status);
+    };
+
+    CheckNetwork();
+
+    const networkListener = Network.addListener(
+      "networkStatusChange",
+      handleNetworkChange
+    );
+
+    return () => {
+      networkListener.remove();
+    };
+  }, []);
+
+  const CurrentNetworkStatus = () => {
+    console.log("network", isOnline);
+
+    if (!isOnline) {
+      console.log("show data modal no internet");
+    }
+  };
 
   const OnchangeValue = (e) => {
     const val = e.target.value;
@@ -15,10 +53,15 @@ function App() {
     }
   };
 
+  const toastify = () => {
+    toast("Form Submitted ");
+  };
+
   const OnHandleSubmit = (e) => {
     e.preventDefault();
 
     if (navigator.onLine) {
+      toastify();
       console.log("form data is sumbimeted", email, password);
     } else {
       setOpen(true);
@@ -32,7 +75,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl text-red-600 flex justify-center mt-4">
-        Online Internet Form
+        Online Internet Form {isOnline}
       </h1>
       <div className="min-h-screen flex items-center flex-col justify-center">
         <form
@@ -69,11 +112,25 @@ function App() {
             <button
               type="submit"
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              onClick={CurrentNetworkStatus}
             >
               Submit
             </button>
           </div>
         </form>
+        <ToastContainer
+          position="top-right"
+          autoClose={4000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss={false}
+          draggable
+          pauseOnHover
+          theme="light"
+          transition:Bounce
+        />
 
         {isOpen && (
           <Modal
@@ -84,6 +141,8 @@ function App() {
             closeModal={closeModal}
           />
         )}
+
+        {isOnline === false && <MobileModal />}
       </div>
     </>
   );
---
+++ src/Components/Mobile/Modal.Mobile.jsx
@@ -0,0 +1,11 @@
+const MobileModal = () => {
+  return (
+    <>
+      <div>
+        <h2>Hello There is no internet in mobile</h2>
+      </div>
+    </>
+  );
+};
+
+export default MobileModal;
