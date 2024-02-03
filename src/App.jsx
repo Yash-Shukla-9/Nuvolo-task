5dab82bc7b227aaa6c2d59eacacf9166fa34f96f
Yash <yashshukla336@gmail.com>
2024-02-03 11:29:23 +0530

    added tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,46 @@
+import { useState } from "react";
+
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const OnchangeValue = (e) => {
+    const val = e.target.value;
+
+    if (e.target.name === "email") {
+      setEmail(val);
+    } else if (e.target.name === "password") {
+      setPassword(val);
+    }
+  };
+
+  const OnHandleSubmit = (e) => {
+    console.log("hello");
+  };
+
   return (
     <>
-      <h1>hello</h1>
-      <h2>isijnk</h2>
+      <form onSubmit={OnHandleSubmit}>
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={OnchangeValue}
+          />
+        </label>
+
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={OnchangeValue}
+          />
+        </label>
+      </form>
     </>
   );
 }
