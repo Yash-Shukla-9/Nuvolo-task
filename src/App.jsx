6fa720ccf79f00dd148ea642bd1c18de38bbd9c1
Yash <yashshukla336@gmail.com>
2024-02-04 09:04:08 +0530

    added new way

--- src/App.jsx
+++ src/App.jsx
@@ -142,7 +142,7 @@ function App() {
           />
         )}
 
-        {isOnline === false && <MobileModal />}
+        {/* {isOnline === false && <MobileModal />} */}
       </div>
     </>
   );
